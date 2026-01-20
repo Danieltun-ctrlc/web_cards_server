949c7823fae70ad0be9a789eedb7ffe4791697c2
Danieltun-ctrlc <danielsbotinfo@gmail.com>
2026-01-20 17:35:52 +0800

    one more

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 
 const dbConfig = {
   host: process.env.DB_HOST,
